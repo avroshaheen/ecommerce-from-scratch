eb4e1c4fd612cc292b85e682630b776e6accdafe
avroshaheen <avrostu@gmail.com>
2025-02-24 22:33:58 +0600

    Comments Added

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,12 @@
+// Get references to interactive menu elements
 const menuBtn = document.getElementById("menuButton");
 const closeBtn = document.getElementById("closeButton");
 const mobileMenu = document.getElementById("mobileMenu");
-
+// Show mobile menu when hamburger button is clicked
 menuBtn.addEventListener("click", () => {
   mobileMenu.classList.remove("hidden");
 });
-
+// Hide mobile menu when close button is clicked
 closeBtn.addEventListener("click", () => {
   mobileMenu.classList.add("hidden");
 });
