533a52c316b6aeea2983f9f237a055b720610ba9
avroshaheen <avrostu@gmail.com>
2025-04-08 23:22:37 +0600

    Surance Commit

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -55,36 +55,15 @@ document.addEventListener("click", (e) => {
 /* -------------------------------------------------------------------------- */
 /*                                  Carousel                                  */
 /* -------------------------------------------------------------------------- */
-let currentIndex = 0;
-const items = document.querySelectorAll('[data-carousel-item]');
-const totalItems = items.length;
-const dots = document.querySelectorAll('[data-carousel-slide-to]');
-
-function showSlide(index) {
-  // Hide all slides
-  items.forEach((item, i) => item.classList.add('hidden'));
-  // Show the selected slide
-  items[index].classList.remove('hidden');
-
-  // Update dots
-  dots.forEach((dot, i) => {
-    dot.setAttribute('aria-current', i === index ? 'true' : 'false');
-  });
-}
-
-// Auto slide every 3 seconds (3000 milliseconds)
-setInterval(() => {
-  currentIndex = (currentIndex + 1) % totalItems;
-  showSlide(currentIndex);
-}, 3000);
-
-// Manual control by clicking on dots
-dots.forEach((dot, i) => {
-  dot.addEventListener('click', () => {
-    currentIndex = i;
-    showSlide(currentIndex);
-  });
+var swiper = new Swiper(".progress-slide-carousel", {
+  loop: true,
+  fraction: true,
+  autoplay: {
+    delay: 1200,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".progress-slide-carousel .swiper-pagination",
+    type: "progressbar",
+  },
 });
-
-// Initial display
-showSlide(currentIndex);
